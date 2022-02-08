8fbd3a2af7b5b367e6298cbfc0e09d5f1963a725
Ryan Flitcroft <ryanflitcroft@ymail.com>
2022-02-07 16:10:05 -0800

    route set for BeaniesPage, BeanieDetail components in App. setState for page onClick of button elements set in BeaniesPage. BeaniesList component called in BeaniesPage. map beanieBabies in BeanieList, calls component BeanieBaby.

--- src/BeanieBaby.js
+++ src/BeanieBaby.js
@@ -1,9 +1,10 @@
 import { Link } from 'react-router-dom';
+import BeanieDetail from './BeanieDetail';
 
 export default function BeanieBaby({ beanieBaby }) {
   return (
     // this should contain a react-router-dom Link to the detail page for this particular beanie baby.
     // it should also render the beanie baby's image and show the beanie baby's name
-    null
+    <BeanieDetail beanieBaby={beanieBaby} />
   );
 }
--- src/BeaniesPage.js
+++ src/BeaniesPage.js
@@ -7,6 +7,8 @@ function App() {
   const [beanieBabies, setBeanieBabies] = useState([]);
   const [page, setPage] = useState(1);
   const perPage = 40;
+
+  // console.log('||', beanieBabies);
   
   useEffect(() => {
     async function fetch() {
@@ -18,7 +20,7 @@ function App() {
     }
 
     fetch();
-  }, []); // what can you do with this array to trigger a fetch every time the page changes?
+  }, [page]); // what can you do with this array to trigger a fetch every time the page changes?
 
   return (
     <>
@@ -26,11 +28,13 @@ function App() {
       <div className='buttons'>
         {/* on click, this button should decrement the page in state  */}
         {/* also, disable this button when you are on the first page */}
-        <button>Previous Page</button>
+        <button onClick={() => setPage(page - 1)}
+          disabled={page === 1}>Previous Page</button>
         {/* on click, this button should increment the page in state  */}
-        <button >Next Page</button>
+        <button onClick={() => setPage(page + 1)}>Next Page</button>
       </div>
       {/* pass the beanie babies into the BeaniesList component */}
+      <BeaniesList beanieBabies={beanieBabies} />
     </>
   );
 }
